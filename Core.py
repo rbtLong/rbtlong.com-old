b9019b17543d272b3310292d427eac4c200baa91
Robert Long <rbtlong@Live.com>
2018-04-01 20:02:03 -0700

    added projects, imgs, and tags relationship; several ui changes

--- Core.py
+++ Core.py
@@ -35,3 +35,13 @@ def projects():
     except ConnectionError as dberr:
         print(f"[/api/projects] Failed: {dberr}")
         return Fail('db')
+
+@corebp.route('/api/imgs/projects')
+def imgs_tag():
+    try:
+        cmd = "select * from rbtlong.imgs where tags like '%projects%'"
+        return Db.Main().rows(Succ, cmd, ())
+
+    except ConnectionError as dberr:
+        print(f"[/api/imgs/name_pronunciation] Failed: {dberr}")
+        return Fail('db')
--- ui/_app/home/home.ctrl.js
+++ ui/_app/home/home.ctrl.js
@@ -11,60 +11,56 @@
         // projects section
         vm.projects = {};
         vm.projects.state = 'loading';
-        vm.projects.data = [
-            {
-                'title': 'Name Pronunciation (Pitzer)',
-                'name': 'Name Pronunciation',
-                'preview_imgs': ['', '', ''],
-                'preview_description':
-                    'This project was for Student Commencement preparation to pronounce students names. It is a replacement ' +
-                    'for a vendor solution for our school. The projected saved annual cost for the school ' +
-                    'while increasing participation seamlessly via the Portal.',
-                'tags': ['AngularJs', 'Web Sockets', 'C#', 'MSSQL']
-            },
-            {
-                'title': 'Course Request (Pitzer)',
-                'name': 'Course Request',
-                'preview_imgs': ['', '', ''],
-                'preview_description':
-                    'Faculties can use this to request courses, over 1,400 courses are available. Curriculum ' +
-                    'Liaisons can lock their department courses, Registrars can review and Administer the course request' +
-                    'process. Documents can be audited to see every change in the version of the document and see what ' +
-                    'fields changed. Locks report the specific courses that changed and the specific fields that was modified. ' +
-                    'This application saved many hours of manual labor and manual process flow.',
-                'tags': ['AngularJs', 'Web Sockets', 'C#', 'MSSQL']
-            },
-            {
-                'title': 'Database Helper (Pitzer)',
-                'name': 'Database Helper',
-                'preview_imgs': ['', '', ''],
-                'preview_description':
-                    'Eliminated a connection leak issue that plagued the portal for many years. Simplified ' +
-                    'database calls using method-chaining and reduced thousands of lines of code.',
-                'tags': ['C#', 'Method-Chaining']
-            },
-            {
-                'title': 'Residence Life Housing Assignment (Pitzer)',
-                'name': 'Reslife Housing Assignment',
-                'preview_imgs': ['', '', ''],
-                'preview_description':
-                    'Eliminated the need for manual assignment and manual indexing of students to rooms. Our ' +
-                    'student body comprises of about 1100 students and 529 possible rooms that can be assigned. ' +
-                    'This project has a visual catalog of the rooms and buildings and all students allowing the assignment' +
-                    'process to be more automated.',
-                'tags': ['Angular 5', 'Web Sockets', 'Go', 'MSSQL', 'Linux', 'Nginx', 'Informix ODBC', 'Active Directory']
-            },
-            {
-                'title': 'Portal X (Pitzer)',
-                'name': 'Portal X',
-                'preview_imgs': ['', '', ''],
-                'preview_description':
-                    'An auxiliary portal that supplements our primary portal. It monitors the visitors on the ' +
-                    'portal Live using Web Sockets. Errors coming from all application can be tracked via the ' +
-                    'auxiliary portal. It also hosts applications such as Residence Life.',
-                'tags': ['Angular 5', 'Web Sockets', 'Go', 'MSSQL', 'Linux', 'Nginx', 'Informix ODBC', 'Active Directory']
+        vm.projects.imgs = [];
+        vm.projects.data = [];
+        vm.projects.get_imgs = function(proj_name) {
+            return vm.projects.imgs
+                .filter(img => {
+                    return img.tags
+                        .map(t => t.toLowerCase())
+                        .filter(t => t == proj_name.toLowerCase())
+                        .length > 0;
+                });
+        };
+
+        $http({
+            method: 'get',
+            url: '/api/projects'
+        }).then(resp => {
+            if(resp.data.hasOwnProperty('success') && resp.data.hasOwnProperty('success')) {
+                vm.projects.data = resp.data.content;
+
+                vm.projects.data.forEach(p => {
+                    if(p.hasOwnProperty('tags') && p.tags) {
+                        try {
+                            p.tags = JSON.parse(p.tags);
+                        } catch(e) {
+                            console.log(`cannot parse project ${p.name}`);
+                        }
+                    }
+                });
             }
-        ];
+        }).catch(err => {
+            console.log(err);
+        });
+
+        $http({
+            method: 'get',
+            url: '/api/imgs/projects'
+        }).then(resp => {
+            if(resp.data.hasOwnProperty('success') && resp.data.hasOwnProperty('success')) {
+                vm.projects.imgs = resp.data.content;
+                vm.projects.imgs.forEach(img => {
+                    try {
+                        img.tags = JSON.parse(img.tags);
+                    } catch(e) {
+                        console.log(`cannot parse img ${p.title}`);
+                    }
+                })
+            }
+        }).catch(err => {
+            console.log(err);
+        });
 
         // skills section
         vm.skills = {};
