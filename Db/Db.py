818c81ddb6e519c903199e472538979106ac97c8
Robert Long <rbtlong@Live.com>
2018-04-23 09:40:40 -0700

    added tags

--- Db/Db.py
+++ Db/Db.py
@@ -1,5 +1,7 @@
 import json
 
+import mysql.connector
+
 from Db.DbConn import DbConn
 
 
@@ -7,11 +9,24 @@ class Db(object):
     db_main = None
 
     @staticmethod
+    def db_main_test(db_main):
+        cnx = mysql.connector.MySQLConnection(user=db_main['user'],
+                                              password=db_main['password'],
+                                              host=db_main['host'],
+                                              database=db_main['database'])
+        cnx.ping()
+        cnx.close()
+        print("[db_main] Connection established.")
+
+    @staticmethod
     def init():
         try:
             data = json.load(open('server.json'))
             Db.db_main = data['db']['main']
             print('[Db Config Main] db info initialized')
+
+            Db.db_main_test(Db.db_main)
+
 
         except ConnectionError as e:
             print(f'[Db Config Main] Error {e.errno} {e.strerror}')
--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import json
 
-import mysql.connector
 from flask import Flask
 
 from Core import corebp
 from Db.Db import Db
 
 app = Flask(__name__)
-
-
-def db_main_test(db_main):
-    cnx = mysql.connector.MySQLConnection(user=db_main['user'],
-                                          password=db_main['password'],
-                                          host=db_main['host'],
-                                          database=db_main['database'])
-    cnx.ping()
-    cnx.close()
-    print("[db_main] Connection established.")
-
 
 if __name__ == '__main__':
 
@@ -30,7 +18,7 @@ if __name__ == '__main__':
         env = data['env']
         print(f'[App Config] app config parsed')
 
-        debug = env == 'prod'
+        debug = env != 'prod'
         print(f"running on host={host} port={port} debug={debug}")
 
         app.register_blueprint(corebp)
